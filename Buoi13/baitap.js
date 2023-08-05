4e30d2c649b1215b7e4130f9d0ff1e46989e50c0
Your Name <you@example.com>
2023-08-06 00:15:27 +0700

    update

--- Buoi13/baitap.js
+++ Buoi13/baitap.js
@@ -3,6 +3,7 @@ class LoadData {
   index = (req, res, path, data) => {
     fs.readFile(`./${path}.html`, "utf8", (err, viewContent) => {
       const result = viewContent.match(/{.+?}/g);
+
       if (result.length) {
         for (let i = 0; i < result.length; i++) {
           const item = result[i];
@@ -12,18 +13,17 @@ class LoadData {
             !Array.isArray(data[itemKey]) &&
             data[itemKey] !== null
           ) {
-            viewContent = viewContent.replaceAll(item, "Contact");
+            let str = "";
             for (let key in data[itemKey]) {
-              viewContent = viewContent
-                .replaceAll(`${itemKey}.${key}`, data[itemKey][key])
-                .replaceAll("{", "")
-                .replaceAll("}", "");
+              str += `<li>${key}: ${data[itemKey][key]}</li>`;
             }
+
+            viewContent = viewContent.replaceAll(item, str);
+          } else {
+            viewContent = viewContent.replaceAll(item, data[itemKey]);
           }
-          viewContent = viewContent.replaceAll(item, data[itemKey]);
         }
       }
-
       res.end(viewContent);
     });
   };
