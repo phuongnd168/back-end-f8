08e869df69018ccc18ec38d9db2ed94a29bb5a61
Your Name <you@example.com>
2023-09-26 23:41:27 +0700

    Nop bai tap buoi 27

---
+++ Buoi27/middlewares/LoginValidate.js
@@ -0,0 +1,13 @@
+const { check } = require('express-validator');
+
+const { where } = require('sequelize');
+module.exports = () => {
+ return [
+
+    check('email', "Email bắt buộc phải nhập").notEmpty(),
+    check('email', "Email không đúng định dạng").isEmail(),
+ 
+    check('password', "Mật khẩu bắt buộc phải nhập").notEmpty(),
+]
+
+}
