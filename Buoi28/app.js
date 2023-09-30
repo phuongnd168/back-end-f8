6a58d4d671898e31980cfe2b57b5b1156af76999
Your Name <you@example.com>
2023-10-01 01:14:32 +0700

    update

--- Buoi28/app.js
+++ Buoi28/app.js
@@ -44,6 +44,7 @@ app.use(express.static(path.join(__dirname, "public")));
     }else{
       if(req.session.role){
         res.redirect("/admin/customers")
+        
       }
      else{
       next()
