087606cb84ef4dac2b9f85551fd219f7f6b3b5dd
Your Name <you@example.com>
2023-11-04 00:04:19 +0700

    update

--- Buoi38/controllers/UserController.js
+++ Buoi38/controllers/UserController.js
@@ -12,14 +12,19 @@ module.exports = {
             fs.mkdirSync("./cache");
         }
         const filePath = path.dirname(__dirname) + `/cache/${cookieValue}.json`
-        fs.appendFile(filePath, JSON.stringify(users), function(err) {
-		    if (err) { 
-                throw err 
-            }else{
-                res.cookie(cookieName, cookieValue, {maxAge: 900000, httpOnly: true});
-                res.render("users/index", {users})
-            }
-        })
+        try {
+            fs.appendFile(filePath, JSON.stringify(users), function(err) {
+                if (err) { 
+                    throw err 
+                }else{
+                    res.cookie(cookieName, cookieValue, {maxAge: 900000, httpOnly: true});
+                    res.render("users/index", {users})
+                }
+            })
+        } catch (error) {
+            console.log(error)
+        }
+      
       
     }
 }
