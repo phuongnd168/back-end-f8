001b636742bf47a940e5e4f3a672de09db5e99cf
Your Name <you@example.com>
2023-11-04 16:23:54 +0700

    update

--- Buoi38/middlewares/UserMiddleware.js
+++ Buoi38/middlewares/UserMiddleware.js
@@ -3,7 +3,10 @@ module.exports = (req, res, next) => {
         if(req.cookies.users){
             try {
                 fs.readFile(`cache/${req.cookies.users}.json`, "utf8", function(err, data){
-                    if(err) throw err;
+                    if(err){
+                        next()
+                        return
+                    }
                     const users = JSON.parse(data)
                     res.render("users/index", {users})
                 });
