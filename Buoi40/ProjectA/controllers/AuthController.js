bcaa8734e5b6a6049dce9e2bdd6c4d02aa58cdeb
Your Name <you@example.com>
2023-11-12 12:41:05 +0700

    update

--- Buoi40/ProjectA/controllers/AuthController.js
+++ Buoi40/ProjectA/controllers/AuthController.js
@@ -77,7 +77,7 @@ module.exports = {
                 deviceIndexes: true,
                 deviceAliasCode: false,
             });
-            const userAgent = 'Mozilla/5.0 (Linux; Android 5.0; NX505J Build/KVT49L) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.78 Mobile Safari/537.36';
+            const userAgent = req.get("User-Agent")
             const result = detector.detect(userAgent);
             if(result){
                 const device = await model.device.findOne({
--- Buoi40/ProjectB/routes/index.js
+++ Buoi40/ProjectB/routes/index.js
@@ -13,7 +13,7 @@ router.get('/', checkMiddleware, AuthMiddleware, async (req, res) => {
         deviceIndexes: true,
         deviceAliasCode: false,
     });
-    const userAgent = 'Mozilla/5.0 (Linux; Android 5.0; NX505J Build/KVT49L) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.78 Mobile Safari/537.36';
+    const userAgent =  req.get("User-Agent")
     const result = detector.detect(userAgent);
     if(result){
         if(!req.cookies.device){
